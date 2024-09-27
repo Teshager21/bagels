c0d550b497869670086d1d017efa23a121315d8c
Teshager <teshager8922@gmail.com>
2024-09-27 16:22:12 +0300

    initial commit. Introduction & prompt

---
+++ bagels.py
@@ -0,0 +1,4 @@
+def bagels():
+    print('Bagels a deductive logic game.\nBy Al Sweigart al@inventwithpython.com')
+    print('I am thinking of a 3-digit number. Try to guess what it is.')
+bagels()
