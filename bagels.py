89aec847ff4fa0aa6d6396dfec00bb16b74633ae
Teshager <teshager8922@gmail.com>
2024-09-27 21:07:02 +0300

    add a function that handles the game logic

--- bagels.py
+++ bagels.py
@@ -1,16 +1,37 @@
 import random
+
+def assess(num,guess):
+    # digits= list(str(num))
+    hint=''
+    for i in range(len(str(guess))):
+        if  str(guess)[i] in str(num):
+            if str(num)[i]==str(guess)[i]:
+                hint+= ' Fermi'
+            else: hint+= ' Pico'
+        else: hint+=' '
+    if hint=='': print("Bagels")
+    elif(hint==' Fermi Fermi Fermi'): 
+        print('You got it!',hint)
+        return 'win'
+    else: print(hint)
+    # print('pico',num)
+
+
 def bagels():
     print('Bagels a deductive logic game.\nBy Al Sweigart al@inventwithpython.com')
     print('I am thinking of a 3-digit number. Try to guess what it is.')
     print('Here are some clues:\nWhen I say:    That means:\n Pico        One digit is correct but in the wrong position.')
     print(' Fermi       One digit is correct and in the right position')
     print(' Bagels      No digit is correct')
-    num= random.randint(100,999)
+  
     print('I have thought up a number.\n    You have 10 guesses to get it.')
-    counter=0
-    while(counter<10):
-        print(f'Guess#{counter+1}')
-        guess=input()
-        counter+=1
-    print(num)
+    while True:
+        num= random.randint(100,999)
+        counter=0
+        while(counter<10):
+            guess=int(input(f'Guess#{counter+1}\n'))
+            if assess(num,guess)=='win': break
+            counter+=1
+        continuePlaying= input('Would you like to continue playing: (yes/no)').lower()
+        if continuePlaying=='no' : break
 bagels()
