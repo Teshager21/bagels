bc078d13c418e7991ce63f37f75ec36626f34ab0
Teshager <teshager8922@gmail.com>
2024-09-27 16:45:01 +0300

    generate a random three digit number

--- bagels.py
+++ bagels.py
@@ -1,8 +1,12 @@
+import random
 def bagels():
     print('Bagels a deductive logic game.\nBy Al Sweigart al@inventwithpython.com')
     print('I am thinking of a 3-digit number. Try to guess what it is.')
     print('Here are some clues:\nWhen I say:    That means:\n Pico        One digit is correct but in the wrong position.')
     print(' Fermi       One digit is correct and in the right position')
     print(' Bagels      No digit is correct')
+    num= random.randint(100,999)
     print('I have thought up a number.\n    You have 10 guesses to get it.')
+
+    print(num)
 bagels()
