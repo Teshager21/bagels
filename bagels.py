c5b8477bfd2f9ddf221761c3220f855e3587709c
Teshager <teshager8922@gmail.com>
2024-09-27 16:31:49 +0300

    game explanation

--- bagels.py
+++ bagels.py
@@ -1,4 +1,8 @@
 def bagels():
     print('Bagels a deductive logic game.\nBy Al Sweigart al@inventwithpython.com')
     print('I am thinking of a 3-digit number. Try to guess what it is.')
+    print('Here are some clues:\nWhen I say:    That means:\n Pico        One digit is correct but in the wrong position.')
+    print(' Fermi       One digit is correct and in the right position')
+    print(' Bagels      No digit is correct')
+    print('I have thought up a number.\n    You have 10 guesses to get it.')
 bagels()
